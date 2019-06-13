d5bdbc8d051e76ead3a3c4d0135cceb65dd89fef
freeintelligence <free04381@gmail.com>
2019-06-13 16:42:09 -0400

    Se completan los tests de 'Utils'.

--- src/framework/utils.test.ts
+++ src/framework/utils.test.ts
@@ -8,4 +8,11 @@ describe('Utils static methods', () => {
     expect(Utils.toTextInt('-100,2', ',')).toEqual('-100.2')
     expect(Utils.toTextInt('-100,2', '.')).toEqual('-1002')
   })
+
+  test('To integer', () => {
+    expect(Utils.toInt('100', ',')).toEqual(100)
+    expect(Utils.toInt('100,92', ',')).toEqual(100.92)
+    expect(Utils.toInt('100.42', ',')).toEqual(10042)
+  })
+
 })
