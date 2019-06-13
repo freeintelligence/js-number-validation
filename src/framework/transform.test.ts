89262986badc2b82f4f562dc37e68e1b956dce7a
freeintelligence <free04381@gmail.com>
2019-06-12 22:39:33 -0400

    Se crea el test para la clase 'Transform'.

---
+++ src/framework/transform.test.ts
@@ -0,0 +1,14 @@
+import { Transform } from './transform.class'
+
+describe('Transform static methods', () => {
+  test('To int', () => {
+    expect(Transform.toInt('100')).toEqual(100)
+    expect(Transform.toInt(100)).toEqual(100)
+    expect(Transform.toInt('-100')).toEqual(-100)
+    expect(Transform.toInt('--')).toBeUndefined()
+  })
+  test('To min', () => {
+    expect(Transform.min('-100', -20)).toEqual(-20)
+    expect(Transform.min(500, -20)).toEqual(500)
+  })
+})
